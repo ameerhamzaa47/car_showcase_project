e69082056033eb34a7a3f3f3d994265823fdf994
Ameer Hamza <hamzaataariq12@gmail.com>
2025-03-12 12:47:43 +0500

    add Satoshi Font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,7 @@ export default function RootLayout({
       <link rel="preconnect" href="https://fonts.googleapis.com" />
 <link rel="preconnect" href="https://fonts.gstatic.com"  />
 <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Yuji+Mai&display=swap" rel="stylesheet"></link>
+<link rel="stylesheet" href="/css/satoshi.css" />
       </head>
       <body
         className="relative"
